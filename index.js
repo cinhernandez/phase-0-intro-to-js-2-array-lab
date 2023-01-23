94c30e696a42789a1b96672e50ed17c54fc20697
Cindy Hernandez <cindyh@mail.com>
2023-01-23 14:35:40 -0800

    commit tree d80df17649a36164d574658dd885a0421852d47f

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+var cats = ["Milo","Otis","Garfield"]; 
+    
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return cats
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    return cats
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats 
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name){
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat(name){
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat(name){
+    return cats.slice(1);
+}
